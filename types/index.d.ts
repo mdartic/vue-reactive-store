caaaa350a09de52d50a216739b37b69672b7d82a
Mathieu DARTIGUES <mathieu@dartic.fr>
2019-05-06 16:29:59 +0200

    Removing types from git

--- types/index.d.ts
+++
@@ -1,3 +0,0 @@
-import { VueReactiveStore } from "./store";
-export { VueReactiveStore };
-export default VueReactiveStore;
--- types/wrapper.d.ts
+++
@@ -1,6 +0,0 @@
-import { VRSHooks } from './typings';
-/**
- * Wrapper for action,
- * allow for triger hooks
- */
-export declare const actionWrapper: (storeName: string, storeState: Object, funcName: string, func: Function, storeHooks: VRSHooks, globalHooks: VRSHooks[]) => (...args: any) => Promise<void>;
