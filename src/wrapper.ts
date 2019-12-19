7d98dec5a25dcddb86c8dea058f70d3dde65acf6
Mathieu DARTIGUES <mathieu@dartic.fr>
2019-12-19 16:00:00 +0100

    Update hook wrapper to return the initial value of the function

--- src/wrapper.ts
+++ src/wrapper.ts
@@ -1,4 +1,4 @@
-// import Vue from 'vue'
+import Vue from 'vue'
 
 // eslint-disable-next-line no-unused-vars
 import { VRSHook, VRSStore } from './typings'
@@ -24,24 +24,47 @@ export const hookWrapper = (
   // wait for initial mutation of store
   // await Vue.nextTick()
 
-  // call all before hooks
-  // and memorize result to pass it to the after hook
-  hooks.forEach(hook => {
-    hook.before && hook.before(store, key, wrapperId)
-  })
+  return new Promise((resolve, reject) => {
+    // call all before hooks
+    // and memorize result to pass it to the after hook
+    hooks.forEach(hook => {
+      hook.before && hook.before(store, key, wrapperId)
+    })
 
-  // we call the initial function with parameters
-  // if it's a promise, we will use await on it
-  const funcResult: Promise<{}>|{} = func(...args)
-  if (funcResult instanceof Promise) {
-    await funcResult
-  }
+    // we call the initial function with parameters
+    // if it's a promise, we will use await on it
+    // const funcResult: Promise<{}>|{} = func(...args)
+    // if (funcResult instanceof Promise) {
+    //   await funcResult
+    // }
 
-  // we wait for the next DOM update (and so the state mutations)
-  // await Vue.nextTick()
+    // we wait for the next DOM update (and so the state mutations)
+    // await Vue.nextTick()
+    // we call the initial function with parameters
+    const fnRes = func(...args)
+    if (fnRes && typeof fnRes.then === 'function') {
+    // it's probably a promise, we work async
+      fnRes.then(async (result: any) => {
+        // we wait for the next DOM update (and so the state mutations)
+        await Vue.nextTick()
 
-  // call all after hooks
-  hooks.forEach(hook => {
-    hook.after && hook.after(store, key, wrapperId)
+        // call all after hooks
+        hooks.forEach(hook => {
+          hook.after && hook.after(store, key, wrapperId)
+        })
+        resolve(result)
+      }).catch(async (error: Error) => {
+        // we wait for the next DOM update (and so the state mutations)
+        await Vue.nextTick()
+
+        // call all after hooks
+        hooks.forEach(hook => {
+          hook.after && hook.after(store, key, wrapperId)
+        })
+        reject(error)
+      })
+    } else {
+      resolve(fnRes)
+    }
   })
 }
